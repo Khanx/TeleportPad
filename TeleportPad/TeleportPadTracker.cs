3ced5ddcd28725fbdcf15c6cec87000f5673bbe1
Khanx Kenovis <maximuskhanx@hotmail.com>
2022-11-09 15:01:04 +0100

    TeleportPad using BlockEntities and now it has a different skin when the teleportpad is paired

---
+++ TeleportPad/TeleportPadTracker.cs
@@ -0,0 +1,142 @@
+﻿using BlockEntities;
+using BlockEntities.Helpers;
+using Chatting;
+using Pipliz;
+using System.Collections.Generic;
+
+namespace TeleportPad
+{
+    [BlockEntityAutoLoader]
+    public class TeleportPadTracker : IEntityManager, IMultiBlockEntityMapping, ILoadedWithDataByPositionType, IChangedWithType
+    {
+        public class TeleportPad : IBlockEntityKeepLoaded, IBlockEntity, IBlockEntitySerializable
+        {
+            public Vector3Int pair = Vector3Int.invalidPos;
+
+            public TeleportPad()
+            {
+                this.pair = Vector3Int.invalidPos;
+            }
+
+            public TeleportPad(Vector3Int pair)
+            {
+                this.pair = pair;
+            }
+
+            public EKeepChunkLoadedResult OnKeepChunkLoaded(Vector3Int blockPosition)
+            {
+                return EKeepChunkLoadedResult.YesLong;
+            }
+
+            public ESerializeEntityResult SerializeToBytes(Chunk chunk, Vector3Byte blockPosition, ByteBuilder builder)
+            {
+                builder.WriteVariable(pair);
+                return ESerializeEntityResult.WroteData | ESerializeEntityResult.LoadChunkOnStartup;
+            }
+        }
+
+        public IEnumerable<ItemTypes.ItemType> TypesToRegister { get { return types; } }
+
+        readonly ItemTypes.ItemType[] types = new ItemTypes.ItemType[]
+        {
+                ItemTypes.GetType("Khanx.TeleportPad"), //Enabled
+                ItemTypes.GetType("Khanx.TeleportPadD") //Disabled
+        };
+
+        public InstanceTracker<TeleportPad> Positions
+        {
+            get;
+            protected set;
+        }
+
+        public TeleportPadTracker()
+        {
+            Positions = new InstanceTracker<TeleportPad>();
+            lastPlaced = new List<(Players.PlayerID, Vector3Int)>();
+        }
+
+        public void OnLoadedWithDataPosition(Chunk chunk, Vector3Int blockPosition, ushort type, ByteReader reader)
+        {
+            if (reader != null)
+            {
+                TeleportPad teleportPad = new TeleportPad(reader.ReadVariableVector3Int());
+                if (chunk.GetOrAllocateEntities().Add(blockPosition, teleportPad))
+                {
+                    Positions.TryAdd(blockPosition, teleportPad);
+                }
+            }
+        }
+
+        public static List<(Players.PlayerID, Vector3Int)> lastPlaced;
+
+        public void OnChangedWithType(Chunk chunk, BlockChangeRequestOrigin requestOrigin, Vector3Int blockPosition, ItemTypes.ItemType typeOld, ItemTypes.ItemType typeNew)
+        {
+            if (requestOrigin.Type != BlockChangeRequestOrigin.EType.Player)
+                return;
+
+            Players.Player player = requestOrigin.AsPlayer;
+
+            //OnRemove
+            if (typeNew == BlockTypes.BuiltinBlocks.Types.air)
+            {
+                int i = lastPlaced.FindIndex(x => x.Item2 == blockPosition);
+
+                if (i != -1)    //Removed lastPlaced
+                    lastPlaced.RemoveAt(i);
+                else if (Positions.TryGetValue(blockPosition, out TeleportPad teleportPad1))    //Removed a teleportPad from a pair
+                {
+                    if (Positions.TryGetValue(teleportPad1.pair, out TeleportPad teleportPad2))
+                    {
+                        teleportPad2.pair = Vector3Int.invalidPos;
+
+                        lastPlaced.Add((player.ID, teleportPad1.pair));
+                        Chat.Send(player, "Removed Teleport Pad, next time that you place a Teleport Pad without disconnecting will connect the other pair of the one that you removed.");
+                        ServerManager.TryChangeBlock(teleportPad1.pair, ItemTypes.GetType("Khanx.TeleportPadD"), player);   //Disabled Skin
+                    }
+                    else
+                        Chat.Send(player, "Removed Teleport Pad");
+                }
+
+                Positions.TryRemove(blockPosition, out _);
+                chunk.GetEntities()?.Remove(blockPosition);
+            }
+
+            //OnAdd
+            if (typeOld == BlockTypes.BuiltinBlocks.Types.air)
+            {
+                int i = lastPlaced.FindIndex(x => x.Item1 == player.ID);
+
+                TeleportPad teleportPad = new TeleportPad();
+
+                if (i == -1) //First pad placed
+                {
+                    lastPlaced.Add((player.ID, blockPosition));
+                    Chat.Send(player, "You need to place a second Teleport Pad without disconnecting to pair both pads.");
+                }
+                else  //Second pad placed
+                {
+                    Positions.TryGetValue(lastPlaced[i].Item2, out TeleportPad teleportPad2);
+
+                    teleportPad.pair = lastPlaced[i].Item2;
+                    teleportPad2.pair = blockPosition; 
+
+                    lastPlaced.RemoveAt(i);
+                    Chat.Send(player, "Teleport Pads paired.");
+
+                    //Enabled Skin
+                    ServerManager.TryChangeBlock(teleportPad.pair, ItemTypes.GetType("Khanx.TeleportPad"), player);
+                    ServerManager.TryChangeBlock(teleportPad2.pair, ItemTypes.GetType("Khanx.TeleportPad"), player);
+
+                    World.GetChunk(teleportPad.pair.ToChunk()).SetDirty();
+                    World.GetChunk(teleportPad2.pair.ToChunk()).SetDirty();
+                }
+
+                if (chunk.GetOrAllocateEntities().Add(blockPosition, teleportPad))
+                {
+                    Positions.TryAdd(blockPosition, teleportPad);
+                }
+            }
+        }
+
+    }
+}
--- TeleportPad/Teleporter.cs
+++ TeleportPad/Teleporter.cs
@@ -1,136 +1,56 @@
 ﻿using BlockEntities;
+using Chatting;
 using ModLoaderInterfaces;
-using Newtonsoft.Json;
 using Pipliz;
 using System.Collections.Generic;
-using System.IO;
-
-using Chatting;
-using Chatting.Commands;
-using System.Linq;
 
 namespace TeleportPad
 {
-    [BlockEntityAutoLoader]
     [ModLoader.ModManager]
-    public class Teleporter : IAfterWorldLoad, IOnQuit, IOnAutoSaveWorld, IChangedWithType, ISingleBlockEntityMapping, IOnPlayerMoved
+    public class Teleporter : IOnPlayerMoved
     {
-        public string teleportPadFile;
-
-        public static List<(Players.PlayerID, Vector3Int)> lastPlaced = new List<(Players.PlayerID, Vector3Int)>();
-        public static Dictionary<Vector3Int, Vector3Int> padPair = new Dictionary<Vector3Int, Vector3Int>();
-
         public static readonly long timeBetweenTeleport = 5000L;
-        public static Dictionary<Players.PlayerID, ServerTimeStamp> lastTeleport = new Dictionary<Players.PlayerID, ServerTimeStamp>();
+        public static Dictionary<Players.PlayerID, (ServerTimeStamp, bool)> lastTeleport = new Dictionary<Players.PlayerID, (ServerTimeStamp, bool)>();
 
-        ItemTypes.ItemType ISingleBlockEntityMapping.TypeToRegister => ItemTypes.GetType("Khanx.TeleportPad");
 
-        public void OnChangedWithType(Chunk chunk, BlockChangeRequestOrigin requestOrigin, Vector3Int blockPosition, ItemTypes.ItemType typeOld, ItemTypes.ItemType typeNew)
-        {
-            //OnRemove
-            if (typeNew == BlockTypes.BuiltinBlocks.Types.air)
-            {
-                int i = lastPlaced.FindIndex(x => x.Item2 == blockPosition);
+        private static TeleportPadTracker tracker = null;
+        public static TeleportPadTracker teleportPadTracker { get { if (tracker == null) { ServerManager.BlockEntityCallbacks.TryGetAutoLoadedInstance(out tracker); } return tracker; } }
 
-                if (i != -1)    //Removed lastPlaced
-                    lastPlaced.RemoveAt(i);
-                else if (padPair.TryGetValue(blockPosition, out Vector3Int pairedPad))
-                {
-                    padPair.Remove(blockPosition);
-                    padPair.Remove(pairedPad);
-                    Chat.SendToConnected("Removed teleport pairs");
-                }
-            }
 
-            //OnAdd
-            if (typeOld == BlockTypes.BuiltinBlocks.Types.air)
-            {
-                if (requestOrigin.Type != BlockChangeRequestOrigin.EType.Player)
-                    return;
-
-                Players.Player player = requestOrigin.AsPlayer;
-
-                int i = lastPlaced.FindIndex(x => x.Item1 == player.ID);
-
-                if(i == -1) //First pad placed
-                {
-                    lastPlaced.Add((player.ID, blockPosition));
-                    Chat.Send(player, "You need to place a second pad teleported without disconecting to pair both pads.");
-                }
-                else //Second pair placed
-                {
-                    Vector3Int pairedPair = lastPlaced[i].Item2;
-                    lastPlaced.RemoveAt(i);
-
-                    padPair.Add(blockPosition, pairedPair);
-                    padPair.Add(pairedPair, blockPosition);
-                    Chat.Send(player, "Pads paired.");
-                }
-            }
-        }
-
-        //Add a time between messages?
         public void OnPlayerMoved(Players.Player player, UnityEngine.Vector3 oldLocation)
         {
             Vector3Int posDown = new Vector3Int(player.Position) + Vector3Int.down;
 
-            if (!padPair.TryGetValue(posDown, out Vector3Int pairedPad))
+            if (!teleportPadTracker.Positions.TryGetValue(posDown, out TeleportPadTracker.TeleportPad teleportPad))
+                return;
+
+            if (teleportPad.pair == Vector3Int.invalidPos)
             {
-                if (World.TryGetTypeAt(posDown, out ItemTypes.ItemType type) && type.Name.Equals("Khanx.TeleportPad"))
+                Chat.Send(player, "Teleport pad not paired.");
+
+                return;
+            }
+
+            if (lastTeleport.TryGetValue(player.ID, out var time) && time.Item1.TimeSinceThis < timeBetweenTeleport)
+            {
+                if (time.Item2 == false)    //Send the message only one time
                 {
-                    Chat.Send(player, "Teleport pad not paired.");
+                    Chat.Send(player, "There is a cooldown of 5s between teleports.");
+
+                    lastTeleport.Remove(player.ID);
+                    lastTeleport.Add(player.ID, (time.Item1, true));
                 }
+
                 return;
             }
 
-            if (lastTeleport.TryGetValue(player.ID, out ServerTimeStamp time) && time.TimeSinceThis < timeBetweenTeleport)
-            {
-                Chat.Send(player, "There is a cooldown of 5s between teleports.");
-                return;
-            }
-
-            Teleport.TeleportTo(player, (pairedPad + Vector3Int.up).Vector);
+            Chatting.Commands.Teleport.TeleportTo(player, (teleportPad.pair + Vector3Int.up).Vector);
             Chat.Send(player, "Teleported");
 
             if (lastTeleport.ContainsKey(player.ID))
                 lastTeleport.Remove(player.ID);
 
-            lastTeleport.Add(player.ID, ServerTimeStamp.Now);
-        }
-
-        public void AfterWorldLoad()
-        {
-            teleportPadFile = "./gamedata/savegames/" + ServerManager.WorldName + "/teleportPad.json";
-
-            if (!File.Exists(teleportPadFile))
-                return;
-
-            var tempPair = JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText(teleportPadFile));
-            padPair = tempPair.ToDictionary(pair => Vector3Int.Parse(pair.Key), pair => Vector3Int.Parse(pair.Value));
-        }
-
-        public void Save()
-        {
-            if (File.Exists(teleportPadFile))
-                File.Delete(teleportPadFile);
-
-            if (padPair.Count == 0)
-                return;
-
-            string json = JsonConvert.SerializeObject(padPair.ToDictionary(pair => pair.Key.ToString(), pair => pair.Value.ToString()));
-
-            File.WriteAllText(teleportPadFile, json);
-            Log.Write("<color=red>Saved Teleport Pads</color>");
-        }
-
-        public void OnQuit()
-        {
-            Save();
-        }
-
-        public void OnAutoSaveWorld()
-        {
-            Save();
+            lastTeleport.Add(player.ID, (ServerTimeStamp.Now, false));
         }
     }
 }
